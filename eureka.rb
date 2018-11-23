d4bbbd8889153404e124e28e24de5694a392c8d8
david <moi@davidwong.fr>
2018-11-22 19:19:17 -0800

    version

--- eureka.rb
+++ eureka.rb
@@ -1,7 +1,7 @@
 class Eureka < Formula
   desc "File Encryption Tool"
   homepage "https://github.com/mimoo/eureka"
-  url "git@github.com:mimoo/eureka.git"
+  url "git@github.com:mimoo/eureka.git", :tag => 'v1.0'
 
   depends_on "go" => :build
 
